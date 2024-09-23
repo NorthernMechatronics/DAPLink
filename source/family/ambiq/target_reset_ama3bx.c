f5165634f8dd436fcfbad2b4401dd029cd7231f1
Joshua Wong <joshua@northernmechatronics.com>
2024-09-23 16:15:01 -0400

    restored reset code

--- source/family/ambiq/target_reset_ama3bx.c
+++ source/family/ambiq/target_reset_ama3bx.c
@@ -44,83 +44,138 @@
 
 uint8_t swd_set_state_ama3bx(target_state_t state)
 {
-    uint32_t val, scratch0, secure, bootldr;
+    uint32_t val, scratch0, secure, jdecpid, bootldr;
     int8_t ap_retries = 2;
+    swd_init();
+    switch (state) {
+        case RESET_HOLD:
+            swd_set_target_reset(1);
+            break;
 
-    if (state == RESET_PROGRAM) {
-        swd_init();
-
-        if (!swd_init_debug()) {
-            return 0;
-        }
-
-        // Enable debug and halt the core (DHCSR <- 0xA05F0003)
-        while (swd_write_word(DBG_HCSR, DBGKEY | C_DEBUGEN ) == 0) {
-            if ( --ap_retries <=0 ) {
-                return 0;
-            }
-            // Target is in invalid state?
+        case RESET_RUN:
             swd_set_target_reset(1);
             osDelay(2);
             swd_set_target_reset(0);
             osDelay(2);
-        }
+            swd_off();
+            break;
 
-        // Wait until core is halted
-        do {
-            if (!swd_read_word(DBG_HCSR, &val)) {
+        case RESET_PROGRAM:
+            if (!swd_init_debug()) {
                 return 0;
             }
-        } while ((val & S_HALT) == 0);
 
-        if (!swd_read_word(MCUCTRL_BOOTLDR, &bootldr)) {
+            // Enable debug and halt the core (DHCSR <- 0xA05F0003)
+            while (swd_write_word(DBG_HCSR, DBGKEY | C_DEBUGEN | C_HALT) == 0) {
+                if (--ap_retries <= 0) {
+                    return 0;
+                }
+                // Target is in invalid state?
+                swd_set_target_reset(1);
+                osDelay(2);
+                swd_set_target_reset(0);
+                osDelay(2);
+            }
+
+            // Wait until core is halted
+            do {
+                if (!swd_read_word(DBG_HCSR, &val)) {
+                    return 0;
+                }
+            } while ((val & S_HALT) == 0);
+
+            // Enable halt on reset
+            //Confirm it is an apollo3
+            if (!swd_read_word(JDEC_PID, &jdecpid)) {
+                return 0;
+            }
+            if ((jdecpid & 0xf0) == 0xc0) {
+                if (!swd_read_word(MCUCTRL_BOOTLDR, &bootldr)) {
+                    return 0;
+                }
+                secure = ((bootldr & 0x0C000000) == 0x04000000);
+            }
+
+            if (secure) {
+                if (!swd_read_word(MCUCTRL_SCRATCH0, &scratch0)) {
+                    return 0;
+                }
+                if (!swd_write_word(MCUCTRL_SCRATCH0, (scratch0 | 0x1))) {
+                    return 0;
+                }
+            } else {
+                if (!swd_write_word(DBG_EMCR, VC_CORERESET)) {
+                    return 0;
+                }
+            }
+
+            // Perform a soft reset
+            if (!swd_read_word(NVIC_AIRCR, &val)) {
+                return 0;
+            }
+
+            if (!swd_write_word(NVIC_AIRCR, VECTKEY | (val & SCB_AIRCR_PRIGROUP_Msk) | SYSRESETREQ)) {
+                return 0;
+            }
+
+            osDelay(10);
+
+            do {
+                if (!swd_read_word(DBG_HCSR, &val)) {
+                    return 0;
+                }
+            } while ((val & S_HALT) == 0);
+
+            // Disable halt on reset
+            if (secure) {
+                if (!swd_write_word(MCUCTRL_SCRATCH0, scratch0)) {
+                    return 0;
+                }
+            } else {
+                if (!swd_write_word(DBG_EMCR, 0)) {
+                    return 0;
+                }
+            }
+
+            break;
+
+        case NO_DEBUG:
+            if (!swd_write_word(DBG_HCSR, DBGKEY)) {
+                return 0;
+            }
+
+            break;
+
+        case DEBUG:
+            if (!JTAG2SWD()) {
+                return 0;
+            }
+
+            if (!swd_write_dp(DP_ABORT, STKCMPCLR | STKERRCLR | WDERRCLR | ORUNERRCLR)) {
+                return 0;
+            }
+
+            // Ensure CTRL/STAT register selected in DPBANKSEL
+            if (!swd_write_dp(DP_SELECT, 0)) {
+                return 0;
+            }
+
+            // Power up
+            if (!swd_write_dp(DP_CTRL_STAT, CSYSPWRUPREQ | CDBGPWRUPREQ)) {
+                return 0;
+            }
+
+            // Enable debug
+            if (!swd_write_word(DBG_HCSR, DBGKEY | C_DEBUGEN)) {
+                return 0;
+            }
+            break;
+
+        default:
             return 0;
-        }
-        secure = ((bootldr & 0xC000000) == 0x04000000);
-
-        if (secure) {
-            if (!swd_read_word(MCUCTRL_SCRATCH0, &scratch0)) {
-                return 0;
-            }
-            if (!swd_write_word(MCUCTRL_SCRATCH0, (scratch0 | 0x1))) {
-                return 0;
-            }
-        } else {
-            if (!swd_write_word(DBG_EMCR, VC_CORERESET)) {
-                return 0;
-            }
-        }
-
-        // Perform a soft reset
-        if (!swd_read_word(NVIC_AIRCR, &val)) {
-            return 0;
-        }
-
-        if (!swd_write_word(NVIC_AIRCR, VECTKEY | (val & SCB_AIRCR_PRIGROUP_Msk) | SYSRESETREQ)) {
-            return 0;
-        }
-
-        osDelay(10);
-
-        do {
-            if (!swd_read_word(DBG_HCSR, &val)) {
-                return 0;
-            }
-        } while ((val & S_HALT) == 0);
-
-        if (secure) {
-            if (!swd_write_word(MCUCTRL_SCRATCH0, scratch0)) {
-                return 0;
-            }
-        } else {
-            if (!swd_write_word(DBG_EMCR, 0)) {
-                return 0;
-            }
-        }
-
     }
 
-    return swd_set_target_state_sw(state);
+    return 1;
 }
 
 const target_family_descriptor_t g_ambiq_ama3bx_family = {
